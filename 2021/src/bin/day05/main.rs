811f70690b93df5975acebdd4f9f170622bed488
bjornomy <bom@myrold.dev>
2021-12-08 00:10:23 +0100

    solve day 5, part 2

--- 2021/src/bin/day05/main.rs
+++ 2021/src/bin/day05/main.rs
@@ -5,6 +5,13 @@ use std::collections::HashMap;
 struct Part1;
 struct Part2;
 
+fn is_diagonal(x1: usize, x2: usize, y1: usize, y2: usize) -> bool {
+    let x_diff = (x1 as i32 - x2 as i32).abs();
+    let y_diff = (y1 as i32 - y2 as i32).abs();
+
+    x_diff == y_diff
+}
+
 #[derive(Default, Debug)]
 struct Line {
     start_x: usize,
@@ -34,8 +41,17 @@ impl Line {
         self.start_x == self.end_x
     }
 
+    fn is_diagonal(&self) -> bool {
+        is_diagonal(self.start_x, self.end_x, self.start_y, self.end_y)
+    }
+
     fn covers(&self) -> Vec<String> {
         let mut covers = Vec::new();
+
+        // covers.push(format!(
+        //     "{},{} -> {},{}",
+        //     self.start_x, self.start_y, self.end_x, self.end_y
+        // ));
 
         let range;
         if self.is_horizontal() {
@@ -58,6 +74,47 @@ impl Line {
                 covers.push(format!("{},{}", self.end_x, y));
             }
         }
+
+        covers
+    }
+
+    fn covers_with_diags(&self) -> Vec<String> {
+        let mut covers = Vec::new();
+
+        if self.is_horizontal() || self.is_vertical() {
+            return self.covers();
+        }
+
+        // covers.push(format!(
+        //     "{},{} -> {},{}",
+        //     self.start_x, self.start_y, self.end_x, self.end_y
+        // ));
+
+        // covers.push(format!("{},{}", -1, -1));
+
+        if self.start_x < self.end_x && self.start_y < self.end_y {
+            // go up and right --> x1 < x2 & y1 < y2
+            for i in 0..(self.end_x - self.start_x + 1) {
+                covers.push(format!("{},{}", self.start_x + i, self.start_y + i));
+            }
+        } else if self.start_x < self.end_x && self.start_y > self.end_y {
+            // go up and left  --> x1 < x2 & y1 > y2
+            for i in 0..(self.end_x - self.start_x + 1) {
+                covers.push(format!("{},{}", self.start_x + i, self.start_y - i));
+            }
+        } else if self.start_x > self.end_x && self.start_y < self.end_y {
+            // go down and right --> x1 > x2 & y1 < y2
+            for i in 0..(self.start_x - self.end_x + 1) {
+                covers.push(format!("{},{}", self.start_x - i, self.start_y + i));
+            }
+        } else if self.start_x > self.end_x && self.start_y > self.end_y {
+            // go down and left  --> x1 > x2 & y1 > y2
+            for i in 0..(self.start_x - self.end_x + 1) {
+                covers.push(format!("{},{}", self.start_x - i, self.start_y - i));
+            }
+        }
+
+        // covers.push(format!("{},{}", -2, -2));
 
         covers
     }
@@ -86,7 +143,22 @@ impl Puzzle for Part1 {
 
 impl Puzzle for Part2 {
     fn solve(input: &str) {
-        println!("NOT STARTED")
+        let count = input
+            .split('\n')
+            .map(|l| Line::from_input(l))
+            .filter(|l| l.is_horizontal() || l.is_vertical() || l.is_diagonal())
+            .flat_map(|l| l.covers_with_diags())
+            // .inspect(|l| println!("{:?}", l))
+            .fold(HashMap::new(), |mut acc, pc| {
+                let counter = acc.entry(pc).or_insert(0);
+                *counter += 1;
+                acc
+            })
+            .values()
+            .filter(|num| **num > 1)
+            .count();
+
+        println!("{}", count)
     }
 }
 
